0c45174f2343287d3efa8c3980a208806c293124
wendt <alexander.wendt@tuwien.ac.at>
2021-12-07 19:05:09 +0100

    Added requirements

--- generate_workspace_efficientdet_server.sh
+++ generate_workspace_efficientdet_server.sh
@@ -75,5 +75,60 @@ fi
 cd $ROOTFOLDER
 source ./venv/$EFFDETENV/bin/activate
 
+
+# Create TF2ODA environment that is used for the evaluation and inference measurements
+TF2ODAENV=tf24_py36
+if [ ! -d "$TF2ODAENV" ] ; then
+  virtualenv -p python3.8 $TF2ODAENV
+  source ./$TF2ODAENV/bin/activate
+
+  # Install necessary libraries
+  python -m pip install --upgrade pip
+  pip install --upgrade setuptools cython wheel
+  
+  # Install EML libraries
+  pip install lxml xmltodict tdqm beautifulsoup4 pycocotools numpy tdqm pandas matplotlib pillow
+  
+  cd $ROOTFOLDER
+  
+  echo # Install protobuf
+  PROTOC_ZIP=protoc-3.14.0-linux-x86_64.zip
+  curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v3.14.0/$PROTOC_ZIP
+  unzip -o $PROTOC_ZIP -d protobuf
+  rm -f $PROTOC_ZIP
+  
+  echo # Clone tensorflow repository
+  git clone https://github.com/tensorflow/models.git
+  cd models/research/
+  cp object_detection/packages/tf2/setup.py .
+  python -m pip install .
+  
+  echo # Add object detection and slim to python path
+  export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+  
+  echo # Prepare TF2 Proto Files
+  ../../protobuf/bin/protoc object_detection/protos/*.proto --python_out=.
+
+  echo # Test installation
+  # If all tests are OK or skipped, then the installation was successful
+  python object_detection/builders/model_builder_tf2_test.py
+  
+  # Install TF2ODA specifics
+  #pip install tensorflow==2.4.1
+  echo #Test if Tensorflow works with CUDA on the machine. For TF2.4.1, you have to use CUDA 11.0
+  python -c "import tensorflow as tf;print(tf.reduce_sum(tf.random.normal([1000, 1000])))"
+  
+  echo "Important information: If there are any library errors, you have to install the correct versions manually. TFODAPI does install the latest version of "
+  echo "tensorflow. However, in this script Tensorflow 2.4.1 is desired. Then, you have to uninstall the newer versions and replace with current versions."
+
+  echo # Installation complete
+  
+else 
+  echo $TF2ODAENV already exists
+fi
+
+cd $ROOTFOLDER
+source ./venv/$EFFDETENV/bin/activate
+
 echo Created environment for AutoML EfficientDet Training and inference
 
